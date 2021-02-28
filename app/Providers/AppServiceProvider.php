11384121209a5ff94e86df2f21200295bb26a9e7
gomgomgit <thechocohead@gmail.com>
2021-02-28 21:26:22 +0700

    repair migrate error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,5 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
         config(['app.locale' => 'id']);
         Carbon::setLocale('id');
+
+        Schema::defaultStringLength(191);
     }
 }
